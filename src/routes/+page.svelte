f0a5604fe8b9306c99eceeb94a6c17d53c66210c
pidanou <pidanoueang@gmail.com>
2023-03-20 22:04:37 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+
+    import Terminal from "./terminal.svelte";
+
+
+</script>
+
+<div class="background"></div>
+
+<Terminal>some text</Terminal>
+
+<style>
+    .background {
+        background-color: black;
+        position: absolute;
+        top: 0;
+        height: 100vh;
+        width: 100%;
+    }
+</style>
---
+++ src/routes/terminal.svelte
@@ -0,0 +1,9 @@
+<div class="terminal">
+    <slot />
+</div>
+
+<style>
+    .terminal {
+        background-color: rgba(0, 0, 0, 0.5);
+    }
+</style>
