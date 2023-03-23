4c1c92eed80d7d736ac9ac36d3660a11f731b66e
Pidanou Eang <pidanoueang@gmail.com>
2023-03-23 19:54:56 +0100

    add responsive

--- src/lib/components/Window.svelte
+++ src/lib/components/Window.svelte
@@ -1,27 +1,68 @@
 <script>
   import { draggable } from "@neodrag/svelte";
+  import MediaQuery from "svelte-media-queries";
 
   export let tabName;
 </script>
 
-<div use:draggable={{ cancel: ".page" }} class="window">
-  <div class="header">
-    <div class="tab"><div id="tabName">{tabName}</div></div>
-    <div class="tributton">
-      <button class="red" />
-      <button class="yellow" />
-      <button class="green" />
+<MediaQuery query="(max-width: 1023px)" let:matches>
+  {#if matches}
+    <div class="window mobile">
+      <div class="header">
+        <div class="tab"><div id="tabName">{tabName}</div></div>
+        <div class="tributton">
+          <button class="red" />
+          <button class="yellow" />
+          <button class="green" />
+        </div>
+      </div>
+      <div class="page">
+        <slot />
+      </div>
     </div>
-  </div>
-  <div class="page">
-    <slot />
-  </div>
-</div>
+  {/if}
+</MediaQuery>
+
+<MediaQuery query="(min-width: 1024px)" let:matches>
+  {#if matches}
+    <div use:draggable={{ cancel: ".page" }} class="window desktop">
+      <div class="header">
+        <div class="tab"><div id="tabName">{tabName}</div></div>
+        <div class="tributton">
+          <button class="red" />
+          <button class="yellow" />
+          <button class="green" />
+        </div>
+      </div>
+      <div class="page">
+        <slot />
+      </div>
+    </div>
+  {/if}
+</MediaQuery>
 
 <style>
   @import url("https://fonts.cdnfonts.com/css/sf-mono");
   :global(button) {
     padding: 0;
+  }
+
+  .window {
+    background-color: rgba(0, 0, 0, 0.9);
+    border-radius: 15px;
+    color: #fff;
+    font-family: "SF Mono", sans-serif;
+    min-width: 635px;
+  }
+  .window.mobile {
+    height: 100%;
+    margin: 0;
+  }
+
+  .mobile .page {
+    height: 100%;
+    width: 100%;
+    margin: 0;
   }
 
   .header {
@@ -69,14 +110,12 @@
     cursor: pointer;
   }
 
-  .window {
+  .window.desktop {
     background-color: rgba(0, 0, 0, 0.9);
     border-radius: 15px;
     height: 80%;
     width: 50%;
     min-width: 700px;
-    color: #fff;
-    font-family: "SF Mono", sans-serif;
   }
 
   .page {
